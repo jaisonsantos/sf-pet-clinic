76ea829e80bfccfc63d66a365b0d30538c59aa3c
Jaison Santos <jaison_js@hotmail.com>
2018-09-10 21:34:15 +0100

    Adding POJOs. Closes #2;

---
+++ src/main/java/com/springframework/sfpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package com.springframework.sfpetclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ src/main/java/com/springframework/sfpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.springframework.sfpetclinic.model;
+
+public class Vet extends  Person {
+
+}
