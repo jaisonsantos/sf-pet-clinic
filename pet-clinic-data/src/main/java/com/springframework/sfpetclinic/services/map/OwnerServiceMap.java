22168f4eef9bf1f9e9ae9d46a8778da35e886903
Jaison Santos <jaison_js@hotmail.com>
2018-09-13 20:08:13 +0100

    Impl Map based Services. Closes #6

---
+++ pet-clinic-data/src/main/java/com/springframework/sfpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,38 @@
+package com.springframework.sfpetclinic.services.map;
+
+import com.springframework.sfpetclinic.model.Owner;
+import com.springframework.sfpetclinic.services.CrudService;
+
+import java.util.Set;
+
+/**
+ * Created by Jaison on 09/13/2018
+ */
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+}
---
+++ pet-clinic-data/src/main/java/com/springframework/sfpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,38 @@
+package com.springframework.sfpetclinic.services.map;
+
+import com.springframework.sfpetclinic.model.Pet;
+import com.springframework.sfpetclinic.services.CrudService;
+
+import java.util.Set;
+
+/**
+ * Created by Jaison on 09/13/2018
+ */
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+}
